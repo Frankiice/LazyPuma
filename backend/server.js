5d2a2f12bee062ea9f1e9a86f9c2a92f14acf071
tiagorosa1511 <tiagorosa1502@gmail.com>
2023-04-07 00:13:22 +0100

    criei o backend para a autenticacao atraves do google

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const passportSetup = require("./passport");
+const authRoute = require("./routes/auth");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+    cookieSession({
+        name: "session",
+        keys:["lazypuma"],
+        maxAge: 24 * 60 * 60 * 100,
+    })
+    );
+    
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin:"http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    }
+));
+
+app.use("/auth", authRoute);
+  
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
 const bcrypt = require("bcryptjs");
