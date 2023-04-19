5f44156f03c1e48255b31c82173a66f10920a2fa
tiagorosa1511 <tiagorosa1502@gmail.com>
2023-04-20 00:27:54 +0100

    lazypuma.com

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(passport.session());
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"http://lazypuma.com",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     }
