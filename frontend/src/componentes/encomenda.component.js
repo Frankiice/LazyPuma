f698e3932a681e73491178ce4ac945d8a9314000
larasangelo <laraangelo.02@gmail.com>
2023-05-07 00:59:50 +0100

    tentativa de meter cart da encomenda a funcionar

--- frontend/src/componentes/encomenda.component.js
+++ frontend/src/componentes/encomenda.component.js
@@ -5,6 +5,7 @@ import { FarBootstrap } from "react-icons/fa";
 
 function Details(props) {
   return (
+  console.log('props:', props),
   <div class="row p-5 mx-5 bg-dark shadow rounded d-block d-sm-flex">
     <div class="p-5 col-md-4 order-md-2 mb-4">
       <h4 class="d-flex justify-content-between align-items-center mb-3">
@@ -12,38 +13,63 @@ function Details(props) {
         <span class="badge badge-secondary badge-pill">3</span>
       </h4>
       <ul class="list-group mb-3">
-        <li class="list-group-item d-flex justify-content-between lh-condensed">
-          <div>
-            <h6 class="my-0">Product name</h6>
-            <small class="text-muted">Brief description</small>
+      {/* <div class="container-cart"> */}
+      {/* <div class="items-carrinho"> */}
+      { props.state.carrinho && props.state.carrinho.map(item => (
+      <div class="container-cart">
+        <div class="carrinho-item" key={item.nome}>
+          <div class="row align-items-center">
+            <div class="col-sm-3">
+              <img class="" style={{minHeight: "30px", minWidth: "30px"}} src={item.img} />
+            </div> 
+            <div class="col-sm-6">
+              <h6>{item.nome}</h6>
+              {/* <small class="text-muted">Brief description</small> */}
+            </div>
+            <div class="col-sm-3">
+              <span class="text-muted">{item.preco}</span>
+            </div>
           </div>
-          <span class="text-muted">$12</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between lh-condensed">
-          <div>
-            <h6 class="my-0">Second product</h6>
-            <small class="text-muted">Brief description</small>
-          </div>
-          <span class="text-muted">$8</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between lh-condensed">
-          <div>
-            <h6 class="my-0">Third item</h6>
-            <small class="text-muted">Brief description</small>
-          </div>
-          <span class="text-muted">$5</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between bg-light">
-          <div class="text-success">
-            <h6 class="my-0">Promo code</h6>
-            <small>EXAMPLECODE</small>
-          </div>
-          <span class="text-success">-$5</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between">
-          <span>Total (USD)</span>
-          <strong>$20</strong>
-        </li>
+        </div>
+      </div>
+    ))}
+
+        {/* </div> */}
+    {/* </div> */}
+          
+          
+       {/* <li class="list-group-item d-flex justify-content-between lh-condensed">
+         <div>
+           <h6 class="my-0">Product name</h6>
+           <small class="text-muted">Brief description</small>
+         </div>
+         <span class="text-muted">$12</span>
+       </li>
+       <li class="list-group-item d-flex justify-content-between lh-condensed">
+         <div>
+           <h6 class="my-0">Second product</h6>
+           <small class="text-muted">Brief description</small>
+         </div>
+         <span class="text-muted">$8</span>
+       </li>
+       <li class="list-group-item d-flex justify-content-between lh-condensed">
+         <div>
+           <h6 class="my-0">Third item</h6>
+           <small class="text-muted">Brief description</small>
+         </div>
+         <span class="text-muted">$5</span>
+       </li>
+       <li class="list-group-item d-flex justify-content-between bg-light">
+         <div class="text-success">
+           <h6 class="my-0">Promo code</h6>
+           <small>EXAMPLECODE</small>
+         </div>
+         <span class="text-success">-$5</span>
+       </li> */}
+       <li class="list-group-item d-flex justify-content-between">
+         <span>Total (USD)</span>
+         <strong>$20</strong>
+       </li>
       </ul>
 
       {/* <form class="card p-2" style={{height:'10%'}}>
@@ -299,6 +325,7 @@ export default class Encomenda extends Component {
       nif: "",
       email: "",
       phone: "",
+      carrinho: JSON.parse(localStorage.getItem('carrinho')) || [],
   };
   this.componentDidMount = this.componentDidMount.bind(this);
 
@@ -367,7 +394,7 @@ getSectionComponent(s) {
     case 0: return <Details state={state}/>;
     case 1: return <Payment />;
     case 2: return <Confirmation />;
-    default: return <Details />;
+    default: return <Details state={state}/>;
   }
 }
 
